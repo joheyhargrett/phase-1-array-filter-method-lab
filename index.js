b40558d3a5db7b43d827c4046d4c0067c562dc32
Johey Hargrett <joheyhargrett@gmail.com>
2025-03-11 16:11:33 -0400

    lab redo filter practice

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(drivers, names = "Johey") {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === names.toLowerCase()
+  );
+}
+
+console.log(findMatching(drivers, "Bobby"));
+
+
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+fuzzyMatch(drivers, "Sa");
+
+
+
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
